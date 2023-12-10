19b81b26f195248891999de9fdba4848e8a9f509
blessedmadukoma <blessedmadukoma@gmail.com>
2023-12-10 23:15:56 +0100

    added NuxtLink and chapters

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -7,18 +7,18 @@
     <h2 class="my-0">{{ lesson.title }}</h2>
 
     <div class="flex space-x-4 mt-2 mb-8">
-      <a
+      <NuxtLink
         v-if="lesson.sourceUrl"
         class="font-normal text-md text-gray-500"
         :href="lesson.sourceUrl"
-        >Download Source Code</a
+        >Download Source Code</NuxtLink
       >
 
-      <a
+      <NuxtLink
         v-if="lesson.downloadUrl"
         class="font-normal text-md text-gray-500"
         :href="lesson.downloadUrl"
-        >Download Video</a
+        >Download Video</NuxtLink
       >
     </div>
 
