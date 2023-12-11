627bd7b4d9d0463147745294bd0a13ff47a62377
blessedmadukoma <blessedmadukoma@gmail.com>
2023-12-11 01:40:07 +0100

    implemented 'mark as complete' using useState

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -25,6 +25,11 @@
     <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
 
     <p>{{ lesson.text }}</p>
+
+    <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
   </div>
 </template>
 
@@ -53,4 +58,32 @@
     // title: title.value,
     title,
   });
+
+  // useState is a reactive global state manager to persist data across pages
+  const progress = useState("progress", () => {
+    return [];
+  });
+
+  const isLessonComplete = computed(() => {
+    // check if there is a chapter array
+    if (!progress.value[chapter.value.number - 1]) {
+      return false;
+    }
+
+    // check if the lesson exists in that chapter
+    if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+      return false;
+    }
+
+    return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+  });
+
+  const toggleComplete = () => {
+    if (!progress.value[chapter.value.number - 1]) {
+      progress.value[chapter.value.number - 1] = [];
+    }
+
+    progress.value[chapter.value.number - 1][lesson.value.number - 1] =
+      !isLessonComplete.value;
+  };
 </script>
